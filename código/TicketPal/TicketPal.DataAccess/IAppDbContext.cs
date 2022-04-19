43de223bd164d4235330978cb1be6cef862d0d5f
lucastroport <federicocastro999@gmail.com>
2022-04-18 21:18:38 -0300

    refactor

---
+++ código/TicketPal/TicketPal.DataAccess/IAppDbContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using TicketPal.Domain.Entity;
+
+namespace TicketPal.DataAccess
+{
+    public interface IAppDbContext
+    {
+        DbSet<UserEntity> Users { get; set; }
+    }
+}
