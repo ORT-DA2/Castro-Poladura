cc0ba8a28b599c90036613c1b12a8ec98e9b9e78
lucastroport <federicocastro999@gmail.com>
2022-05-01 01:20:23 -0300

    added repository mocks

---
+++ código/TicketPal.Tests/TicketPal.BusinessLogic.Tests/Services/BaseServiceTest.cs
@@ -0,0 +1,33 @@
+
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using TicketPal.Interfaces.Repository;
+
+namespace TicketPal.BusinessLogic.Tests.Services
+{
+    [TestClass]
+    public abstract class BaseServiceTest
+    {
+        protected Mock<DbContext> mockDbContext;
+        protected IMapper mapper;
+        protected Mock<IUserRepository> usersMock;
+
+
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            // Context mock
+            this.mockDbContext = new Mock<DbContext>(MockBehavior.Loose);
+            // Mapper mock
+            var mapperConfig = new MapperConfiguration(cfg => cfg.AddProfile<AutoMapping>());
+            this.mapper = mapperConfig.CreateMapper();
+
+            // Repositories mock
+            this.usersMock = new Mock<IUserRepository>();
+        }
+
+    }
+}
