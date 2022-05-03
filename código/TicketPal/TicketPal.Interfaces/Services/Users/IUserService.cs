b3b22f87bdf6580bb851ecd29dc5a10b0fd40afa
lucastroport <federicocastro999@gmail.com>
2022-05-03 20:26:42 -0300

    rename package

--- código/TicketPal/TicketPal.Interfaces/Services/Users/IUserService.cs
+++ código/TicketPal/TicketPal.Interfaces/Services/Users/IUserService.cs
@@ -1,14 +1,16 @@
 using System.Collections.Generic;
 using TicketPal.Domain.Constants;
+using TicketPal.Domain.Models.Request;
+using TicketPal.Domain.Models.Response;
 
-namespace TicketPal.Interfaces.Services.User
+namespace TicketPal.Interfaces.Services.Users
 {
     public interface IUserService
     {
         User Login(AuthenticationRequest model);
         IEnumerable<User> GetUsers(UserRole role = UserRole.SPECTATOR);
         User GetUser(int id);
-        OperationResult SignUp(SignInRequest model);
+        OperationResult SignUp(SignUpRequest model);
         OperationResult UpdateUser(UpdateUserRequest model, UserRole authorization = UserRole.SPECTATOR);
         OperationResult DeleteUser(int id);
     }
