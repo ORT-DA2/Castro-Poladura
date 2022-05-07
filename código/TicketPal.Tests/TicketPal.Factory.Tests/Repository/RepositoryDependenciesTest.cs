252e942a2f7ebdceecdfa17d69f82e94a4d9d8e7
lucastroport <federicocastro999@gmail.com>
2022-05-07 13:25:29 -0300

    creted getrepository test for getting concrete repository when calling with specified type

--- código/TicketPal.Tests/TicketPal.Factory.Tests/Repository/RepositoryDependenciesTest.cs
+++ código/TicketPal.Tests/TicketPal.Factory.Tests/Repository/RepositoryDependenciesTest.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicketPal.DataAccess.Repository;
 using TicketPal.Domain.Entity;
 using TicketPal.Interfaces.Repository;
 
@@ -29,36 +30,46 @@ namespace TicketPal.Factory.Tests.Repository
         [TestMethod]
         public void UserRepositoryDependencyCheck()
         {
-            var repository = serviceProvider.GetService<IGenericRepository<UserEntity>>();
+            var repository = factory.GetRepository(typeof(UserEntity));
+
             Assert.IsNotNull(repository);
+            Assert.IsTrue(repository.GetType() == typeof(UserRepository));
         }
 
         [TestMethod]
         public void ConcertRepositoryDependencyCheck()
         {
-            var repository = serviceProvider.GetService<IGenericRepository<ConcertEntity>>();
+            var repository = factory.GetRepository(typeof(ConcertEntity));
+
             Assert.IsNotNull(repository);
+            Assert.IsTrue(repository.GetType() == typeof(ConcertRepository));
         }
 
         [TestMethod]
         public void GenreRepositoryDependencyCheck()
         {
-            var repository = serviceProvider.GetService<IGenericRepository<GenreEntity>>();
+            var repository = factory.GetRepository(typeof(GenreEntity));
+
             Assert.IsNotNull(repository);
+            Assert.IsTrue(repository.GetType() == typeof(GenreRepository));
         }
 
         [TestMethod]
         public void PerformerRepositoryDependencyCheck()
         {
-            var repository = serviceProvider.GetService<IGenericRepository<PerformerEntity>>();
+            var repository = factory.GetRepository(typeof(PerformerEntity));
+
             Assert.IsNotNull(repository);
+            Assert.IsTrue(repository.GetType() == typeof(PerformerRepository));
         }
 
         [TestMethod]
         public void TicketRepositoryDependencyCheck()
         {
-            var repository = serviceProvider.GetService<IGenericRepository<TicketEntity>>();
+            var repository = factory.GetRepository(typeof(TicketEntity));
+
             Assert.IsNotNull(repository);
+            Assert.IsTrue(repository.GetType() == typeof(TicketRepository));
         }
     }
 }
