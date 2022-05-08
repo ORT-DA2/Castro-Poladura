be4e8d139cf7a211cb67ec38c93268c0ac5f6da9
lucastroport <federicocastro999@gmail.com>
2022-05-08 16:02:16 -0300

    model bindings test

---
+++ código/TicketPal.Tests/TicketPal.BusinessLogic.Tests/Filters/ModelValidateFilterTest.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using TicketPal.BusinessLogic.Filters.Model;
+
+namespace TicketPal.BusinessLogic.Tests.Filters
+{
+    [TestClass]
+    public class ModelValidatFilterTest
+    {
+        private ModelValidateFilter filter;
+
+        [TestMethod]
+        public void EnsureCreatedNotNull()
+        {
+            filter = new ModelValidateFilter();
+            var modelState = new ModelStateDictionary();
+            modelState.AddModelError("name", "invalid");
+
+            var actionContext = new ActionContext(
+                Mock.Of<HttpContext>(),
+                new Microsoft.AspNetCore.Routing.RouteData(),
+                new Microsoft.AspNetCore.Mvc.Abstractions.ActionDescriptor(),
+                modelState);
+
+            var actionExecutingContext = new ActionExecutingContext(
+                actionContext,
+                new List<IFilterMetadata>(),
+                new Dictionary<string, object>(),
+                Mock.Of<Controller>()
+            );
+
+            filter.OnActionExecuting(actionExecutingContext);
+        }
+
+    }
+}
