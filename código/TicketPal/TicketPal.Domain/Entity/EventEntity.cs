85d5c42fe65d4fb51a10a69261b424ec3e65dbef
lucastroport <federicocastro999@gmail.com>
2022-05-23 20:43:39 -0300

    implemented properties

--- código/TicketPal/TicketPal.Domain/Entity/EventEntity.cs
+++ código/TicketPal/TicketPal.Domain/Entity/EventEntity.cs
@@ -9,5 +9,8 @@ namespace TicketPal.Domain.Entity
         public decimal TicketPrice { get; set; }
         public string CurrencyType { get; set; }
         public string EventType { get; set; }
+        public string Location { get; set; }
+        public string Address { get; set; }
+        public string Country { get; set; }
     }
 }
