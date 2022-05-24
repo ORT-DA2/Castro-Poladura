39cec0e9af74be2741e1610ac2f007f37b39c656
lucastroport <federicocastro999@gmail.com>
2022-05-23 23:43:27 -0300

    added missing mapping tests

--- código/TicketPal.Tests/TicketPal.BusinessLogic.Tests/Mapper/PerformerMappingTest.cs
+++ código/TicketPal.Tests/TicketPal.BusinessLogic.Tests/Mapper/PerformerMappingTest.cs
@@ -1,10 +1,42 @@
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicketPal.BusinessLogic.Mapper;
+using TicketPal.Domain.Constants;
+using TicketPal.Domain.Entity;
+using TicketPal.Domain.Models.Response;
 
 namespace TicketPal.BusinessLogic.Tests.Mapper
 {
     [TestClass]
     public class PerformerMappingTest
     {
-        
+        [TestMethod]
+        public void PerformerEntityToPerformerMapperTest()
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<AutoMapping>());
+            var mapper = config.CreateMapper();
+
+            var toBeMapped = new PerformerEntity
+            {
+                Id = 1,
+                UserInfo = new UserEntity { Id = 1, Firstname = "someName" },
+                Members = new List<UserEntity>(),
+                Genre = new GenreEntity { Name = "someGenre" },
+                PerformerType = Constants.PERFORMER_TYPE_BAND,
+                StartYear = "1968"
+            };
+
+            var performer = mapper.Map<Performer>(toBeMapped);
+
+            Assert.IsNotNull(performer);
+            Assert.IsTrue(performer.StartYear.Equals(toBeMapped.StartYear)
+                && performer.UserInfo.Firstname.Equals(toBeMapped.UserInfo.Firstname)
+                && performer.PerformerType.Equals(toBeMapped.PerformerType)
+                && performer.Id == toBeMapped.Id
+            );
+            CollectionAssert.AreEquivalent(toBeMapped.Members, performer.Members.ToList());
+        }
     }
 }
