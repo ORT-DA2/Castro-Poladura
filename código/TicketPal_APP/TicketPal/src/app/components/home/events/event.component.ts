ce043015dcda324250a56d6b7f090f16dcda5b19
Ricardo Poladura <ricardopoladurapignata@gmail.com>
2022-06-10 01:10:30 -0300

    Added Event detail button functionality

--- código/TicketPal_APP/TicketPal/src/app/components/home/events/event.component.ts
+++ código/TicketPal_APP/TicketPal/src/app/components/home/events/event.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { IConcert } from 'src/app/models/response/concert.model';
 import { TokenStorageService } from 'src/app/services/storage/token-storage.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-event',
@@ -21,4 +22,15 @@ export class EventComponent implements OnInit {
     this.userLoggedIn = !!this.tokenService.getToken()
   }
 
+  showConcertDetails(id: string): void {
+    var concertSelected = this.concerts.find(c => c.id == id);
+    var info = 'Tour name: ' + concertSelected?.tourName + '. Event type: ' + concertSelected?.eventType 
+    + '. Date: ' + concertSelected?.date + '. Available tickets: ' + concertSelected?.availableTickets 
+    + '. Ticket price: ' + concertSelected?.currencyType + ' ' + concertSelected?.ticketPrice 
+    + '. Location: ' + concertSelected?.address + ', ' + concertSelected?.location + ' - ' + concertSelected?.country + '.';
+    Swal.fire({
+      title: 'Concert details:',
+      text: info,
+    })
+  }
 }
